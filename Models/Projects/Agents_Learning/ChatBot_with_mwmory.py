24a72b95ef3a78653792a3be589694a47ee87928
Pravesh Kumar <pkr17072000@gmail.com>
2026-08-18 21:55:42 +0530

    ConversationlBufferMemory_Chatbot

---
+++ Models/Projects/Agents_Learning/ChatBot_with_mwmory.py
@@ -0,0 +1,27 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_classic.chains import LLMChain
+from langchain_classic.memory.buffer import ConversationBufferMemory
+
+load_dotenv()
+
+
+model = ChatOpenAI(model="gpt-4o")
+
+memory = ConversationBufferMemory(
+    memory_key="chat_history",
+    input_key="input",
+    output_key="text",
+    return_messages=True
+)
+
+prompt = PromptTemplate(template="Chat History: {chat_history}\n\n Human: {input}\nAI:",input_variables=["input"],memory_template = memory)
+
+conversation_chain = LLMChain(llm=model,prompt = prompt, memory = memory,output_key="text")
+
+query = input("Enter your Query : ")
+while not query == "exit":
+    result = conversation_chain.invoke({"input":query})
+    print(result["text"])
+    query = input("Enter your next Query : ")
