870c821b0bf653f7743cd22c55072e955bd3c211
Pravesh Kumar <pkr17072000@gmail.com>
2026-08-19 22:43:58 +0530

    Weather Agent

---
+++ Models/Projects/Weather_Agent/weatherTool.py
@@ -0,0 +1,39 @@
+from typing import List, Union
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+from langchain_core.tools import tool
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_classic.agents import AgentExecutor, create_tool_calling_agent
+
+from langchain_community.utilities import OpenWeatherMapAPIWrapper
+from langchain_community.tools.openweathermap.tool import OpenWeatherMapQueryRun
+from langchain_community.document_loaders.weather import WeatherDataLoader
+
+
+def now_ist_str()->str:
+    ist = ZoneInfo("Asia/Kolkata")
+    return datetime.now(ist).strftime("%Y-%m-%d %H:%M IST")
+
+@tool
+def get_weather(city:str)->str:
+    """Returns a short,human-friendly weather report for the given city(mock date)."""
+    if not city or not city.strip():
+        return "Please provide a city name."
+    key = city.strip().lower()
+    # data = 
+    weather_wrapper = OpenWeatherMapAPIWrapper()
+    data = WeatherDataLoader(client=weather_wrapper,places=city)
+    # weather_tool = OpenWeatherMapQueryRun(api_wrapper=weather_wrapper)
+
+    return (
+        f"City : {city.strip().title()}\n"
+        f"Time : {now_ist_str()}\n"
+        f"Condition : {data["condition"]}"
+        f"Temprature : {data["tempF"]}"
+        f"Humidity : {data["humidity"]}"
+        f"windMPH : {data["windMPH"]}"
+    )
+
+
